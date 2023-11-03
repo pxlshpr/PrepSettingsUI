44b103c44454dab32663f05de1d2bba20c16cbd6
Ahmed Khalaf <pxlshpr@gmail.com>
2023-11-03 21:24:39 +0500

    tag increment

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
             targets: ["PrepSettingsUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/pxlshpr/PrepShared", from: "0.0.143"),
-        .package(url: "https://github.com/pxlshpr/PrepSettings", from: "0.0.3"),
+        .package(url: "https://github.com/pxlshpr/PrepShared", from: "0.0.163"),
+        .package(url: "https://github.com/pxlshpr/PrepSettings", from: "0.0.4"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
